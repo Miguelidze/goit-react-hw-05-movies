0bd180802675b335790779a0436e0543dde007af
Miguelidze <migueldeorpesa@gmail.com>
2022-04-19 19:30:58 +0200

    add netlify and fix basename

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import {BrowserRouter} from 'react-router-dom';
-// import App from './App';
-// import 'modern-normalize/modern-normalize.css';
-// import './index.css';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode >
-//     <BrowserRouter >
-//       <App/>
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
@@ -22,10 +6,10 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode >
+  < React.StrictMode basename = "/goit-react-hw-05-movies/">
     <BrowserRouter >
       <App/>
     </BrowserRouter>
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
